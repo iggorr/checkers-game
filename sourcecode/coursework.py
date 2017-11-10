5d16c6a99e11be223717727175956d6ad383118d
Igors Ahmetovs <40125689@live.napier.ac.uk>
2017-11-10 13:59:21 +0000

    Added piece moving functionality

--- sourcecode/coursework.py
+++ sourcecode/coursework.py
@@ -1,4 +1,3 @@
-
 # Defining board size and colour variables
 SIZE = 8
 EMPTY = "·"
@@ -7,26 +6,27 @@ WHITE = "O"
 
 letter_row = ""
 
+
 # Function to initialise the board
 def initialise_board():
-
     # Empty board
     board = [[EMPTY] * SIZE for i in range(SIZE)]
 
     # Populating the board with pieces based on the row and column
     for row in range(0, SIZE):
         # Black pieces in the first rows/2-1 rows
-        if row < SIZE/2-1:
+        if row < SIZE / 2 - 1:
             populate_row(board, row, BLACK)
         # White pieces in the last rows/2-1 rows
-        elif row > SIZE/2:
+        elif row > SIZE / 2:
             populate_row(board, row, WHITE)
 
     global letter_row
-    for i in range(ord('A'), ord('A')+SIZE):
+    for i in range(ord('A'), ord('A') + SIZE):
         letter_row += chr(i)
 
     return board
+
 
 def populate_row(board, row, piece):
     if row % 2 == 0:
@@ -38,64 +38,134 @@ def populate_row(board, row, piece):
             if element % 2 == 0:
                 board[row][element] = piece
 
+
 # Function to print a board
 def print_board(board):
-
     print('   ', ' '.join(letter_row))
     for row in range(0, SIZE):
-        print(row+1, ' ', ' '.join([element for element in board[row]]), ' ', row+1)
+        print(row + 1, ' ', ' '.join([element for element in board[row]]), ' ', row + 1)
     print('   ', ' '.join(letter_row))
 
-def check_move(move):
+
+def play_players(board):
+    turn = BLACK
+    while turn == BLACK:
+        print_board(board)
+        move = input("\nBlack's move: ")
+        if make_move(board, move, turn):
+            turn = WHITE
+    else:
+        print_board(board)
+        move = input("\nWhite's move: ")
+        if make_move(board, move, turn):
+            turn = BLACK
+
+
+def make_move(board, move, piece):
+
+    # Checking the length of the move
     if len(move) > 7:
         print("Move description too long, please try again!")
         return False
-
     if len(move) < 5:
         print("Move description too short, please try again!")
         return False
 
-    first_column = ord(move[0].upper())-65
+    # Checking the first column of the move
+    first_column = ord(move[0].upper()) - 65
     if first_column not in range(0, SIZE):
-        print("Wrong first letter, please try again!")
+        print("Wrong first column, please try again!")
         return False
 
+    # Checking the first row of the move
     try:
-        first_row = int(move[1])
+        first_row = int(move[1]) - 1
+        if first_row < 0:
+            print("Row must be 1-9, please try again!")
+            return False
     except ValueError:
-        print("Wrong first number, please try again!")
+        print("Wrong first row, please try again!")
         return False
 
-    if move[2] != '-':
-        if first_row == 1:
-            try:
-                first_row = int(move[1:3])
-                if first_row > SIZE:
-                    print("Number limit exceeded, please try again!")
-                    return False
-                # stop here...
-            except ValueError:
-                print("Dash symbol not found, please try again!")
+    # Checking for the dash symbol
+    if move[2] == '-':
+        expected_dash = move[2]
+    elif first_row == 0:
+        try:
+            first_row = int(move[1:3]) - 1
+            if first_row > SIZE:
+                print("Number limit exceeded, please try again!")
                 return False
-        else:
-            print("Dash symbol not found, please try again!")
+            expected_dash = move[3]
+        except ValueError:
+            expected_dash = ''
+    else:
+        expected_dash = ''
+    if expected_dash != '-':
+        print("Dash symbol not found, please try again!")
+        return False
+
+    # Checking the second column of the move
+    if first_row < 9:
+        second_column = ord(move[3].upper()) - 65
+    else:
+        second_column = ord(move[4].upper()) - 65
+    if second_column not in range(0, SIZE):
+        print("Wrong second column, please try again!")
+        return False
+
+    # Checking the second row of the move
+    if first_row < 9:
+        try:
+            second_row = int(move[4]) - 1
+            if second_row < 0:
+                print("Row must be 1-9, please try again!")
+                return False
+        except ValueError:
+            print("Wrong second row, please try again!")
             return False
+    else:
+        try:
+            second_row = int(move[5]) - 1
+            if second_row < 0:
+                print("Row must be 1-9, please try again!")
+                return False
+        except ValueError:
+            print("Wrong second row, please try again!")
+            return False
+
+    # Checking whether piece is present
+    if board[first_row][first_column] != piece:
+        print("Piece not found, please try again!")
+        return False
+
+    # Checking that the piece was moved diagonally, by one square
+    column_difference = second_column - first_column
+    if column_difference != 1 and column_difference != -1:
+        print("Piece must be move by one square diagonally, please try again!")
+        return False
+
+    # Checking that the piece was moved in the right direction
+    if piece == BLACK:
+        if second_row - first_row != 1:
+            print("Black piece must be moved by one square forward, please try again!")
+            return False
+
+    else:
+        if second_row - first_row != -1:
+            print("White piece must be moved by one square forward, please try again!")
+            return False
+
+    board[first_row][first_column] = EMPTY
+    board[second_row][second_column] = piece
 
     return True
 
-def play_players():
-    turn = BLACK
-    while turn == BLACK:
-        move = input("\nBlack's move: ")
-        if check_move(move):
-            turn = BLACK
-    else:
-        move = input("\nWhite's move: ")
 
 def main():
     board = initialise_board()
-    print_board(board)
-    play_players()
+    play_players(board)
+
 
 if __name__ == "__main__":
     main()
