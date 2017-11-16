1c35bbcd9dd64dcbe0fac74c7e0cd23be2a2378b
Igors Ahmetovs <40125689@live.napier.ac.uk>
2017-11-16 00:35:01 +0000

    Added more move legality checks. Added piece-capturing functionality and kinging

--- sourcecode/coursework.py
+++ sourcecode/coursework.py
@@ -1,15 +1,21 @@
-# Defining board size and colour variables
+import random, time
+
+# Defining board size and piece symbol variables
 SIZE = 8
 EMPTY = "·"
 BLACK = "@"
+BLACK_KING = "B"
 WHITE = "O"
+WHITE_KING = "W"
 
 letter_row = ""
+black_count = int((SIZE - 2) * 0.25 * SIZE)
+white_count = int((SIZE - 2) * 0.25 * SIZE)
 
 
 # Function to initialise the board
 def initialise_board():
-    # Empty board
+    # Creating an empty two-dimensional list to store the pieces
     board = [[EMPTY] * SIZE for i in range(SIZE)]
 
     # Populating the board with pieces based on the row and column
@@ -21,6 +27,7 @@ def initialise_board():
         elif row > SIZE / 2:
             populate_row(board, row, WHITE)
 
+    # Printing the letter row
     global letter_row
     for i in range(ord('A'), ord('A') + SIZE):
         letter_row += chr(i)
@@ -28,7 +35,9 @@ def initialise_board():
     return board
 
 
+# Function to fill a row with pieces
 def populate_row(board, row, piece):
+    # Based on the row, put the pieces into the right cell
     if row % 2 == 0:
         for element in range(0, SIZE):
             if element % 2 != 0:
@@ -39,7 +48,7 @@ def populate_row(board, row, piece):
                 board[row][element] = piece
 
 
-# Function to print a board
+# Function to print the board with letter rows and number columns
 def print_board(board):
     print('   ', ' '.join(letter_row))
     for row in range(0, SIZE):
@@ -47,23 +56,58 @@ def print_board(board):
     print('   ', ' '.join(letter_row))
 
 
-def play_players(board):
+# Function for playing an actual game of checkers
+def play_game(board, mode):
+    # Always set the first turn to black
     turn = BLACK
-    while turn == BLACK:
+
+    # Game loop until one of the sides loses all the pieces
+    while black_count > 0 and white_count > 0:
+        # Printing the number of pieces for each of the sides
+        print("Black: ", black_count, "White: ", white_count)
+        # Printing the board
         print_board(board)
-        move = input("\nBlack's move: ")
-        if make_move(board, move, turn):
+
+        if turn == BLACK:
+            # If mode is Player vs Player, ask for input
+            if mode == 1:
+                move = input("\nBlack's move: ")
+                # If user's input is invalid, try again
+                if not user_move(board, move, turn):
+                    continue
+            # If black are played by the computer, generate a move
+            else:
+                computer_move(board, turn)
+            # Give the next round to White
             turn = WHITE
-    else:
-        print_board(board)
-        move = input("\nWhite's move: ")
-        if make_move(board, move, turn):
+
+        else:
+            # If mode is not PC vs PC, ask for input
+            if mode != 3:
+                move = input("\nWhite's move: ")
+                # If user's input is invalid, try again
+                if not user_move(board, move, turn):
+                    continue
+            # If white are played by the computer, generate move
+            else:
+                computer_move(board, turn)
+            # Give the next round to Black
             turn = BLACK
 
+    # If game reaches an end, announce the winner
+    else:
+        print("Black: ", black_count, "White: ", white_count)
+        print_board(board)
+        if white_count == 0:
+            print("Black won!")
+        else:
+            print("White won!")
 
-def make_move(board, move, piece):
 
-    # Checking the length of the move
+# Function to check and execute user's move
+def user_move(board, move, turn):
+
+    # Checking the length of the string
     if len(move) > 7:
         print("Move description too long, please try again!")
         return False
@@ -134,37 +178,208 @@ def make_move(board, move, piece):
             print("Wrong second row, please try again!")
             return False
 
-    # Checking whether piece is present
-    if board[first_row][first_column] != piece:
-        print("Piece not found, please try again!")
-        return False
-
-    # Checking that the piece was moved diagonally, by one square
-    column_difference = second_column - first_column
-    if column_difference != 1 and column_difference != -1:
-        print("Piece must be move by one square diagonally, please try again!")
-        return False
-
-    # Checking that the piece was moved in the right direction
-    if piece == BLACK:
-        if second_row - first_row != 1:
-            print("Black piece must be moved by one square forward, please try again!")
+    # Checking whether piece is present on the selected square
+    piece = board[first_row][first_column]
+    if turn == BLACK:
+        if piece != BLACK and piece != BLACK_KING:
+            print("Can only move own pieces, please try again!")
+            return False
+    else:
+        if piece != WHITE and piece != WHITE_KING:
+            print("Can only move own pieces, please try again!")
             return False
 
-    else:
-        if second_row - first_row != -1:
+    column_difference = second_column - first_column
+    row_difference = second_row - first_row
+
+    # Checking that the piece was moved in the right direction if not a king
+    if piece == BLACK:
+        if row_difference <= 0:
+            print("Black piece must be moved forward, please try again!")
+            return False
+
+    elif piece == WHITE:
+        if row_difference >= 0:
             print("White piece must be moved by one square forward, please try again!")
             return False
 
-    board[first_row][first_column] = EMPTY
-    board[second_row][second_column] = piece
+    # Checking that the target square is vacant
+    target_square = board[second_row][second_column]
+    if target_square != EMPTY:
+        print("Target square is occupied, please try again!")
+        return False
+
+    # Checking if the target row is a king's row
+    if piece == BLACK:
+        if second_row == SIZE - 1:
+            piece = BLACK_KING
+    elif piece == WHITE:
+        if second_row == 0:
+            piece = WHITE_KING
+
+    # Checking whether the move is a capture attempt
+    if abs(column_difference) == 2 and abs(row_difference) == 2:
+        piece_to_capture = board[first_row + int(row_difference / 2)][first_column + int(column_difference / 2)]
+
+        # Checking that the piece to be captured is not empty
+        if piece_to_capture == EMPTY:
+            print("Piece must be moved by one square only, please try again!")
+            return False
+
+        # Checking that the piece to be captured in not of the same colour
+        if turn == BLACK:
+            if piece_to_capture == BLACK or piece_to_capture == BLACK_KING:
+                print("Can't jump over own pieces, please try again!")
+                return False
+        else:
+            if piece_to_capture == WHITE or piece_to_capture == WHITE_KING:
+                print("Can't jump over own pieces, please try again!")
+                return False
+
+        # Proceed with the capture
+        board[first_row][first_column] = EMPTY
+        board[first_row + int(row_difference / 2)][first_column + int(column_difference / 2)] = EMPTY
+        board[second_row][second_column] = piece
+        if turn == BLACK:
+            global white_count
+            white_count -= 1
+        else:
+            global black_count
+            black_count -= 1
+
+    else:
+        # If a regular move, check that the piece was moved diagonally by one square
+        if abs(column_difference) != 1 or abs(row_difference) != 1:
+            print("Piece must be move by one square diagonally, please try again!")
+            return False
+
+        board[first_row][first_column] = EMPTY
+        board[second_row][second_column] = piece
 
     return True
 
 
+# Function to generate a move for the computer
+def computer_move(board, turn):
+
+    # This needs reworked obvy
+
+    while True:
+        first_column = chr(random.choice(range(0, SIZE)) + 65)
+        first_row = random.choice(range(0, SIZE)) + 1
+        second_column = chr(random.choice(range(0, SIZE)) + 65)
+        second_row = random.choice(range(0, SIZE)) + 1
+
+        move = "{}{}-{}{}".format(first_column, first_row, second_column, second_row)
+        if computer_check(board, move, turn):
+            print(move)
+            break
+
+    return
+
+
+# Function to check and execute computer's move
+def computer_check(board, move, turn):
+
+    first_column = ord(move[0].upper()) - 65
+    first_row = int(move[1]) - 1
+
+    if first_row < 9:
+        second_column = ord(move[3].upper()) - 65
+    else:
+        second_column = ord(move[4].upper()) - 65
+
+    if first_row < 9:
+        second_row = int(move[4]) - 1
+    else:
+        second_row = int(move[5]) - 1
+
+    # Checking whether piece is present on the selected square
+    piece = board[first_row][first_column]
+    if turn == BLACK:
+        if piece != BLACK and piece != BLACK_KING:
+            return False
+    else:
+        if piece != WHITE and piece != WHITE_KING:
+            return False
+
+    column_difference = second_column - first_column
+    row_difference = second_row - first_row
+
+    # Checking that the piece was moved in the right direction if not a king
+    if piece == BLACK:
+        if row_difference <= 0:
+            return False
+
+    elif piece == WHITE:
+        if row_difference >= 0:
+            return False
+
+    # Checking that the target square is vacant
+    target_square = board[second_row][second_column]
+    if target_square != EMPTY:
+        return False
+
+    # Checking if the target row is a king's row
+    if piece == BLACK:
+        if second_row == SIZE - 1:
+            piece = BLACK_KING
+    elif piece == WHITE:
+        if second_row == 0:
+            piece = WHITE_KING
+
+    # Checking whether the move is a capture attempt
+    if abs(column_difference) == 2 and abs(row_difference) == 2:
+        piece_to_capture = board[first_row + int(row_difference / 2)][first_column + int(column_difference / 2)]
+
+        # Checking that the piece to be captured is not empty
+        if piece_to_capture == EMPTY:
+            return False
+
+        # Checking that the piece to be captured in not of the same colour
+        if turn == BLACK:
+            if piece_to_capture == BLACK or piece_to_capture == BLACK_KING:
+                return False
+        else:
+            if piece_to_capture == WHITE or piece_to_capture == WHITE_KING:
+                return False
+
+        # Proceed with the capture
+        board[first_row][first_column] = EMPTY
+        board[first_row + int(row_difference / 2)][first_column + int(column_difference / 2)] = EMPTY
+        board[second_row][second_column] = piece
+        if turn == BLACK:
+            global white_count
+            white_count -= 1
+        else:
+            global black_count
+            black_count -= 1
+
+    else:
+        # If a regular move, check that the piece was moved diagonally by one square
+        if abs(column_difference) != 1 or abs(row_difference) != 1:
+            return False
+
+        board[first_row][first_column] = EMPTY
+        board[second_row][second_column] = piece
+
+    return True
+
+# Function that executes the move
+def make_move():
+    return True
+
+
 def main():
+    # Initialise the board
     board = initialise_board()
-    play_players(board)
+    # Prompt the user for game type
+    mode = int(input("Player vs Player (1)"
+                     "\nPlayer vs PC (2)"
+                     "\nPC vs PC (3)"
+                     "\nYour Choice: "))
+    # Play the game in selected mode
+    play_game(board, mode)
 
 
 if __name__ == "__main__":
